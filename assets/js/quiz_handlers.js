51c2e497785ac44451a7e6b9939f70cb39dcca93
Qing <qzou3@g.ucla.edu>
2021-07-11 22:52:58 -0700

    Add way to cycle through quiz questions and determeine if they are correct

--- assets/js/quiz_handlers.js
+++ assets/js/quiz_handlers.js
@@ -1,3 +1,4 @@
+
 var questions = [
     {
         question: 'Commonly Used Data Types DO NOT include:',
@@ -13,19 +14,61 @@ var questions = [
         question: 'Arrays in Javascript can be used to store',
         options: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
         answer: 3
+    },
+    {
+        question: 'A very useful tool used during development and debugging for printing context to the debugger is:',
+        options: ['JavaScript', 'Terminal/Bash', 'For Loops', 'Console Log'],
+        answer: 3
     }
 ]
 
+var questionIndex = 0;
 
-var mainElement = document.querySelector('main');
 
 document.addEventListener('DOMContentLoaded', function() {
+    var mainElement = document.querySelector('main');
+
     var startSection = document.getElementById('start-section')
     var quizSection = document.getElementById('quiz-section')
     var startQuizBtn = document.getElementById('start-quiz-btn')
+    var questionHeader = document.getElementById('question-header')
+    var answerOptionsList = document.getElementById('answer-options')
+    var options = answerOptionsList.querySelectorAll('li')
+    var answerResult = document.getElementById('answer-result')
+
+    function populateQuestion(index) {
+        var question0 = questions[index]
+        questionHeader.textContent = question0.question
+        var question0Options = question0.options
+        for (let i = 0; i < options.length; i++) {
+            options[i].textContent = question0Options[i]
+        }
+    }
+
+
     startQuizBtn.addEventListener('click', function() {
         startSection.classList.add('hide')
         quizSection.classList.remove('hide')
-        
+        populateQuestion(0)
     })
+
+    function optionClickHandler(index) {
+        let result
+        if (index === questions[questionIndex].answer) {
+            result = 'Correct!'
+        } else {
+            result = 'Wrong!'
+        }
+        answerResult.textContent = result
+        questionIndex++
+        setTimeout(function() {
+        populateQuestion(questionIndex)
+            answerResult.textContent = ''
+        }, 2000)
+    }
+
+    for (let i = 0; i < options.length; i++) {
+        options[i].addEventListener('click', () => optionClickHandler(i) )
+    }
+
 })
